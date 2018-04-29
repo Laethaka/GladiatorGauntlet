96faeba6ce67d51a1892e4e7afc5e1b3372e06ed
Jesse <laethaka@gmail.com>
2018-04-29 16:19:16 -0700

    everything works but game reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,14 +9,16 @@ class Gladiator {
 }
 
 //GLADIATOR OBJECTS SETUP
-var thraex = new Gladiator('Thraex', 110, 10, 15);
-var retarius = new Gladiator('Retarius', 90, 15, 15);
-var murmilla = new Gladiator('Murmilla', 135, 5, 15);
-var maximus = new Gladiator('Maximus', 120, 20, 15);
+var thraex = new Gladiator('Thraex', 100, 16, 15);
+var retarius = new Gladiator('Retarius', 70, 22, 15);
+var murmilla = new Gladiator('Murmilla', 110, 14, 15);
+var maximus = new Gladiator('Maximus', 90, 18, 15);
 
 //OTHER VARIABLES SETUP
 var hero = {};
 var enemy = {};
+var levelUp = 0;
+var enemiesRemaining = 3;
 // var opponentsArr = [thraex, retarius, murmilla, maximus];
 var heroChosen = false;
 var enemyChosen = false;
@@ -29,22 +31,22 @@ $('#lossSpan').text(losses);
 function thraexUpdate() {
     $(`#thraexHeader`).text(thraex.name);
     $(`#thraex #gladiatorHealth`).text(thraex.health);
-    $('#thraexAttack').text(thraex.attack);
+    $('#thraex #gladiatorAttack').text(thraex.attack);
 };
 function retariusUpdate() {
     $(`#retariusHeader`).text(retarius.name);
     $(`#retarius #gladiatorHealth`).text(retarius.health);
-    $('#retariusAttack').text(retarius.attack);
+    $('#retarius #gladiatorAttack').text(retarius.attack);
 };
 function murmillaUpdate() {
     $(`#murmillaHeader`).text(murmilla.name);
     $(`#murmilla #gladiatorHealth`).text(murmilla.health);
-    $('#murmillaAttack').text(murmilla.attack);
+    $('#murmilla #gladiatorAttack').text(murmilla.attack);
 };
 function maximusUpdate() {
     $('#maximusHeader').text(maximus.name);
     $(`#maximus #gladiatorHealth`).text(maximus.health);
-    $('#maximusAttack').text(maximus.attack);
+    $('#maximus #gladiatorAttack').text(maximus.attack);
 };
 
 //CALLS TO SET INITIAL VALUES ON DOM
@@ -52,6 +54,28 @@ thraexUpdate();
 retariusUpdate();
 murmillaUpdate();
 maximusUpdate();
+
+//GAME RESET FUNCTION
+function gameReset() {
+    $('#headerImg').attr('src', 'assets/images/r_gladiators.jpg')
+    $('.opponentClass').removeClass('opponentClass');
+    $('.heroClass .healthLine').html('Health: <span id="gladiatorHealth"></span>');
+    $('.heroClass').removeClass('heroClass');
+    $('.deadClass').removeClass('deadClass');
+    $('.gladiator .healthLine').removeClass('invisible');
+    $('#instructions').text('Pick a gladiator to be your champion in the arena!');
+    heroChosen = false;
+    enemyChosen = false;
+    thraex = new Gladiator('Thraex', 100, 16, 15);
+    retarius = new Gladiator('Retarius', 70, 22, 15);
+    murmilla = new Gladiator('Murmilla', 110, 14, 15);
+    maximus = new Gladiator('Maximus', 90, 18, 15);
+    thraexUpdate();
+    retariusUpdate();
+    murmillaUpdate();
+    maximusUpdate();
+    $('#gameResetBtn').addClass('invisible');
+};
 
 //GLADIATOR FOCUS PIC STYLING
 $('.gladiatorPic').on('mouseenter', event => {
@@ -71,35 +95,43 @@ $('.gladiatorPic').on('click', function() {
     if (!heroChosen) {//HERO PICKED
         $('#instructions').text('Choose your opponent!');
         heroChosen = true;
-        $('.headerImg').fadeOut('slow');
+        $('#headerImg').fadeOut('slow');
         if ($(this).attr('id') === 'thraexPic') { ///PLAYER CHOSE THRAEX
             hero = thraex;
+            levelUp = thraex.attack;
             $('#thraex').addClass('heroClass');
+            $('#thraexPic').addClass('heroPic');
             $('#retarius, #murmilla, #maximus').addClass('opponentClass');
             $('#thraexHealth').addClass('heroHealth');
-            $('.opponentClass .attackLine').addClass('invisible');
+            // $('.opponentClass .attackLine').addClass('invisible');
         } else if ($(this).attr('id') === 'retariusPic') { //PLAYER CHOSE RETARIUS
             hero = retarius;
+            levelUp = retarius.attack;
             $('#retarius').addClass('heroClass');
+            $('#retariusPic').addClass('heroPic');
             $('#thraex, #murmilla, #maximus').addClass('opponentClass')
             $('#retariusHealth').addClass('heroHealth');
-            $('.opponentClass .attackLine').addClass('invisible');
+            // $('.opponentClass .attackLine').addClass('invisible');
         } else if ($(this).attr('id') === 'murmillaPic') { //PLAYER CHOSE MURMILLA
             hero = murmilla;
+            levelUp = murmilla.attack;
             $('#murmilla').addClass('heroClass');
+            $('#murmillaPic').addClass('heroPic');
             $('#retarius, #thraex, #maximus').addClass('opponentClass')
             $('#murmillaHealth').addClass('heroHealth');
-            $('.opponentClass .attackLine').addClass('invisible');
+            // $('.opponentClass .attackLine').text('Strength: 15');
         } else if ($(this).attr('id') === 'maximusPic') { //PLAYER CHOSE MAXIMUS
             hero = maximus;
-            $('#maximus').addClass('heroClass md-auto');
+            levelUp = maximus.attack;
+            $('#maximus').addClass('heroClass');
+            $('#maximusPic').addClass('heroPic');
             $('#retarius, #murmilla, #thraex').addClass('opponentClass')
             $('#maximusHealth').addClass('heroHealth');
-            $('.opponentClass .attackLine').addClass('invisible');
+            // $('.opponentClass .attackLine').addClass('invisible');
         }
         $('.heroClass').animate ({bottom: '+=200px'}, 'slow');
 
-    } else if ((!enemyChosen) && heroChosen) {//ENEMY PICKED; DOES NOT PREVENT ENEMY FROM BEING HERO
+    } else if ((!enemyChosen) && heroChosen && (enemiesRemaining > 0 && ($(this).attr('class') !== 'card-img-top gladiatorPic mx-auto heroPic'))) {//ENEMY PICKED; DOES NOT PREVENT ENEMY FROM BEING HERO
         $('#instructions').text('Engage in glorious combat!')
         enemyChosen = true;
         $('#attackBtn').removeClass('invisible');
@@ -130,23 +162,44 @@ function combat() {
     //HERO HITS FIRST
     enemy.health -= hero.attack; 
     $('.enemyClass #gladiatorHealth').text(enemy.health);
+    hero.attack += levelUp;
+    $('.heroClass #gladiatorAttack').text(hero.attack);
         if (enemy.health <= 0) { //ENEMY KILLED
             $('.enemyClass').animate ({top: '+=200px'}, 'slow');
             $('.enemyClass .gladiatorHeader').text("DEAD");
             $('.enemyClass .healthLine').addClass('invisible');
             $('.enemyClass').removeClass('enemyClass').addClass('deadClass');
             enemyChosen = false;
+            enemiesRemaining -= 1;
             $('#attackBtn').addClass('invisible');
             $('#instructions').text('Choose your next opponent!');
+                if (enemiesRemaining == 0) { //GAME WIN
+                    wins++;
+                    $('#winSpan').text(wins);
+                    $('#instructions').text('A glorious victory!');
+                    $('.heroClass .healthLine').text('WINNER');
+                    $('#gameResetBtn').removeClass('invisible');
+                    $('.heroClass').animate ({top: '+=200px'}, 'slow');
+                    $('#headerImg').attr('src', 'assets/images/victory.jpg')
+                    $('#headerImg').fadeIn('slow');
+                };
         };
     //ENEMY HITS BACK
     hero.health -= enemy.attack;
     $('.heroClass #gladiatorHealth').text(hero.health);
-        if (hero.health <= 0) { //HERO KILLED
-            $('.heroClass').removeClass('heroClass');
-            $('.enemyClass').removeClass('enemyClass');
+        if ((hero.health <= 0) && (enemiesRemaining > 0)) { //GAME LOSS
+            $('.enemyClass').animate ({top: '+=200px'}, 'slow');
+            $('.heroClass').animate ({top: '+=200px'}, 'slow');
+            $('.enemyClass').removeClass('enemyClass').addClass('opponentClass');
             losses++;
             $('#lossSpan').text(losses);
+            $('#instructions').text('A humiliating defeat!');
+            $('.heroClass .gladiatorHeader').text("YOU DIED");
+            $('.heroClass').addClass('deadClass');
+            $('#attackBtn').addClass('invisible');
+            $('#gameResetBtn').removeClass('invisible');
+            $('#headerImg').attr('src', 'assets/images/defeat.png')
+            $('#headerImg').fadeIn('slow');
         }
 }
 
